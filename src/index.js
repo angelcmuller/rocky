d359d351dac49d453640dc7d72c74d008e92378a
ramirezaaronjob <aaronjobr@nevada.unr.edu>
2022-11-15 11:00:03 -0800

    seperated the map page from app.js. still wonky tho

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
