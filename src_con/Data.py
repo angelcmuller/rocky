f27ca129078e5d5dd47d32cfd60102c79b7830f0
Duelahan <tristanbaileyse@gmail.com>
2022-12-04 01:10:32 -0800

    Removed API key and made an object for managing database additions

--- src_con/Data.py
+++ src_con/Data.py
@@ -4,11 +4,12 @@ import numpy as np
 # import tensorflow as tf
 # import matplotlib.pyplot as plt
 import csv
-import pandas as pd
-import pymongo
 import requests
-import certifi
+import pandas as pd
+from dotenv import load_dotenv
+import os
 
+from Data_Manager import Data_Manager
 
 def main():
     print("========Welcome to ROS Backend========")
@@ -17,27 +18,22 @@ def main():
 
 
 def addPinData():
-
     # Obtain the the data from CSV file
-    df = pd.read_csv('src_con\Data.csv', usecols=[
-        'Lattitude', 'Longitude', 'Classification', 'Degree'])
-    print(df)
-
-    # Convert the data into a series of records
-    data = df.to_dict(orient="records")
-    print(data)
-
-    # Connecting to the databse
-    client = pymongo.MongoClient("mongodb://tristanbailey:RockyRoadKey2022@ac-ap9bbel-shard-00-00.fpy1pqs.mongodb.net:27017,ac-ap9bbel-shard-00-01.fpy1pqs.mongodb.net:27017,ac-ap9bbel-shard-00-02.fpy1pqs.mongodb.net:27017/?ssl=true&replicaSet=atlas-zrbeo7-shard-0&authSource=admin&retryWrites=true&w=majority",  tlsCAFile=certifi.where())
-
-    # Make a database
-    db = client["pinDatabase"]
+    df = pd.read_csv('src_con/Data.csv', usecols=[
+        'Pid', 'Lattitude', 'Longitude', 'Altitude', 'MeasurementDate',
+        'UploadDate', 'Source', 'Cid', 'Classification', 'Degree'])
+    print(df) 
+    load_dotenv()   
+    data_manager = Data_Manager(os.environ["MONGOAPIUSER"], os.environ["MONGOAPIKEY"])
 
     # Add data to the database
     # Note Pins is a custom name for the connection (you can rename it)
-    db.Pins.insert_many(data)
-    print("Pin cordinates added to database")
+    for index, row in df.iterrows():
+        data_manager.add(row['Pid'], row['Lattitude'], row['Longitude'], row['Altitude'],
+            row['MeasurementDate'], row['UploadDate'], row['Source'], row['Cid'], row['Classification'], row['Degree'])
+    
+    data_manager.push()
 
-
+#main guard
 if __name__ == "__main__":
     main()
