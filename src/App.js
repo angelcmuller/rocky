3565cfa7fe6a80d5e588605e53114168a38a0f67
Angel Carranco Muller <101151695+angelcmuller@users.noreply.github.com>
2022-12-07 18:56:11 -0800

    Landing Page.. testing Map Continue Button to redirect to Map.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,108 @@
 import React from 'react'
-import {
-  BrowserRouter as Router, 
-  Switch,
-  Route,
-  Navigate,
-  useNavigate,
-  Routes
-} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Map from './Map.js'
 import './App.css';
+import logo from './Logo.png'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup, Wrap, WrapItem } from '@chakra-ui/react'
+import { Switch, FormControl, FormLabel, Stack } from '@chakra-ui/react'
 
-function App() {
-  const navigate = useNavigate();
-  const [showResults, setShowResults] = React.useState(true)
-  const navigatetoMap = () =>{
-    setShowResults(current => !current);
-    navigate('/Map')
+// Main class where the React App starts
+// Landing Page
+class App extends React.Component {
+  render () {
+    return (
+        // Using ChakraProvider for components
+        <ChakraProvider>
+          <div id='body'>
+            <br />
+            <br />
+            <br />
+            <h1 id='welcome'> Welcome to Rocky Road Project </h1>
+            <br /><br />
+            <Introduction />
+            <br /><br />
+            <Form />
+            <ColorB />
+          </div>
+        </ChakraProvider>
+      )
   }
-
-  return (
-    
-    <div>
-      {showResults &&(
-    <div>
-    {
-    //put landing page here 
-    }
-        <button onClick = {navigatetoMap}>Enter</button>
-    </div>
-      )}
-    <div>
-    <Routes>
-    <Route path="/Map" element={<Map />} />
-    </Routes></div>
-    </div>
-  );
 }
 
-export default App;
+// Displayment of Logo and Intro description of what Rocky Road is about
+const Introduction = (props) => {
+  return(
+    <div>
+      <img src={logo} id='Logo'/>
+      <br /><br /><br />
+      <p> Rocky Road is here to provide useful information that will prevent road based
+        inconveniences, injuries, and fatalities.
+      </p>
+      <p>
+      Thank you for joining us in this project.
+      </p>
+    </div>
+  )
+}
+
+// Events related to the Button
+const Form = (props) => {
+  const [mapisShown, setmapIsShown] = useState(false);
+
+  const [homepageShown, sethomepageisShown] = useState(true);
+  
+  const showMap= event => {
+    setmapIsShown(current => !current);
+    sethomepageisShown(current => !current);
+  };
+  
+  const navigate = useNavigate();
+
+  const [showResults, setShowResults] = React.useState(true)
+
+  const navigatetoMap = () => {
+    setShowResults(current => !current);
+    navigate('Map.js')
+  }
+  
+  return(
+    <div>
+      <Button colorScheme='teal' size='lg' onClick={navigatetoMap}>
+        Start your Journey..
+      </Button>
+    </div>
+  )
+}
+
+// Color Blind switch and events related to it
+const ColorB = (props) => {
+  const [isHovering, setIsHovering] = useState(false)
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  }
+
+  return(
+    <div id='colorblind'>
+      <br />
+      <FormControl display='flex' alignItems='center'>
+        <FormLabel htmlFor='email-alerts' mb='0'>
+          Color Blind
+        </FormLabel>
+        <Switch id='color-blind' size='lg' colorScheme='orange'
+        onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>
+
+        {isHovering && <p id='auxToggle'>B&W Map option for Color Blind disability</p>}
+      </FormControl>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <ChakraProvider theme={theme}>
       <App />
-    </ChakraProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>, 
+  rootElement
+  )
