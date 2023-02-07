0bfbb12b95302625d0be6e99298438d1b295c32b
Angel Carranco Muller <101151695+angelcmuller@users.noreply.github.com>
2023-02-07 15:42:25 -0800

    testing out map styles changes

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,5 @@
 import JsonListReturn from "./components/recordList";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import {
   Box,
   Button,
@@ -12,8 +13,13 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
-  PopoverCloseButton,
+  PopoverHeader,
   PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  Portal,
+  PopoverAnchor,
   Menu,
   MenuButton,
   MenuList,
@@ -21,9 +27,14 @@ import {
   MenuItemOption,
   AlertDialog,
   Center,
+  Radio,
+  RadioGroup,
+  useBoolean,
 } from '@chakra-ui/react'
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { FaLocationArrow, FaCarAlt,FaTimes,FaCommentAlt, FaCalendar, FaCloud, FaEyeSlash, FaEye, FaBlind, FaServer} from 'react-icons/fa'
 import './App.css'
+import './Map.css'
 import { useRef, useState, useMemo, useEffect} from 'react'
 import RequestMap from "./Request";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
@@ -64,6 +75,16 @@ function Map() {
 
     map.addControl(new mapboxgl.FullscreenControl());
 
+    const layerList = document.getElementById('menu');
+    const inputs = layerList.getElementsByTagName('input');
+    
+    for (const input of inputs) {
+      input.onclick = (layer) => {
+        const layerId = layer.target.id;
+        map.setStyle('mapbox://styles/mapbox/' + layerId);
+      };
+    }
+
     //displaying marker onto map
     const marker = new mapboxgl.Marker()
       .setLngLat([-119.81, 39.529],
@@ -76,16 +97,85 @@ function Map() {
     };
   });
 
+  //function to select Map Style Angel C. Muller
+  function WithPopoverAnchor() {
+    const [isEditing, setIsEditing] = useBoolean()
+    const [color, setColor] = React.useState('')
+  
+    return (
+      <Popover
+        isOpen={isEditing}
+        onOpen={setIsEditing.on}
+        onClose={setIsEditing.off}
+        closeOnBlur={false}
+        isLazy
+        lazyBehavior='keepMounted'
+      >
+        <HStack>
+          <PopoverAnchor>
+            <Input
+              color={color}
+              w='auto'
+              display='inline-flex'
+              isDisabled={!isEditing}
+              defaultValue='Color Blind?'
+            />
+          </PopoverAnchor>
+  
+          <PopoverTrigger>
+            <Button h='40px' colorScheme='orange'>
+              {isEditing ? 'Save' : 'Edit'}
+            </Button>
+          </PopoverTrigger>
+        </HStack>
+  
+        <PopoverContent>
+          <PopoverBody>
+            Select a new Map Style:
+            <RadioGroup value={color} onChange={(newColor) => setColor(newColor)}>
+              <Radio value='red'> Streets </Radio>
+              <Radio value='blue'> Light </Radio>
+              <Radio value='green'> Dark </Radio>
+              <Radio value='purple'> outdoors </Radio>
+            </RadioGroup>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    )
+  }
+
   return (
     <Flex  position= 'fixed' height = '100vh' w='100vw' display = 'vertical' color='white'>
-      <Center  position = 'relative' h='100' bg='green.500'>
-        <Text>Box 1</Text>
+      <Center  position = 'relative' h='50' bg='green.300'>
+        <div id="menu">
+          <input id="streets-v12" type="radio" name="rtoggle" value="streets"/>
+          <label for="streets-v12"> streets </label>
+          <input id="light-v11" type="radio" name="rtoggle" value="light" />
+          <label for="light-v11"> light </label>
+          <input id="dark-v11" type="radio" name="rtoggle" value="dark"/>
+          <label for="dark-v11"> dark </label>
+          <input id="outdoors-v12" type="radio" name="rtoggle" value="outdoors"/>
+          <label for="outdoors-v12"> outdoors </label>
+        </div>
+        <Text>
+          Box1
+        </Text>
+        <WithPopoverAnchor/>
+        <Menu>
+          <MenuButton as={IconButton} aria-label='Options' icon={<HamburgerIcon />} variant='outline'/>
+            <MenuList>
+              <MenuItem> Contact Road Side Assistance </MenuItem>
+              <MenuItem> Something else </MenuItem>
+              <MenuItem> Make a Comment </MenuItem>
+            </MenuList>
+        </Menu>
       </Center>
       
       <HStack spacing = '0' >
         <Box bg='red' h ='100' w = '20%'> Descriptions</Box>
         
         <div ref={mapContainer} className="map-container" style={{width: 'auto', height: 'auto'}}/>
+
       </HStack>
     </Flex>
   );
