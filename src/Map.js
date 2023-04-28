418dc32991f7c83c613a8211d7b75f8ab9debf01
Duelahan <tristanbaileyse@gmail.com>
2023-04-27 22:45:03 -0700

    fixed bug where liking one comment locked users out of ever liking another one

--- src/Map.js
+++ src/Map.js
@@ -408,7 +408,7 @@ function Map() {
               const { lng, lat } = marker.getLngLat();
               const value = 1; // user clicked "like"
               Like(lat, lng, value); 
-            }, { once: true });
+            });
 
             dislikeBtn.addEventListener('click', () => {
               const { lng, lat } = marker.getLngLat();
@@ -420,7 +420,8 @@ function Map() {
               //   timer: 2000,
               //   showConfirmButton: false
               // });
-            }, { once: true });
+            });
+          //need to add one:true to keep pop up from liking multiple times in one click
           }, { once: true });
 
         }
