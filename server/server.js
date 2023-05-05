79abe35d9f533db7b64e7648cfc0546e110e2264
Ubuntu <ubuntu@ip-172-31-19-164.us-west-2.compute.internal>
2023-05-05 00:38:33 +0000

    production server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-app.use(require("./routes/LogMongo"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-// This method was implemented by Gabriel and uses a modified version from the MongoDB Mern tutorial (https://www.mongodb.com/languages/mern-stack-tutorial)
+var express = require('express');
+var config = require('./server/config/config');
+var app = require('./server/server');
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+const DB = await mongoose.connect(‘mongodb://localhost:27017/databaseName’, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+
+const https = require('https');
+const fs = require('fs');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/projectrockyroad.com/privkey.pem', 'utf8'); // key
+const certificate = fs.readFileSync('/etc/letsencrypt/live/projectrockyroad.com/cert.pem', 'utf8'); // certificate
+const ca = fs.readFileSync('/etc/letsencrypt/live/projectrockyroad.com/chain.pem', 'utf8'); // chain
+const credentials = {
+   key: privateKey,
+   cert: certificate,
+   ca: ca
+};
+app.use(express.static('public'));
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen('8443', () => {
+    console.log('listening on https://projectrockyroad.com:8443');
 });
